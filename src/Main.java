01172951e58cf301fd3cb2fe4fffbb8d64c28e28
BowearpCT <c.kunapakornkarn@hotmail.com>
2018-10-18 11:51:14 +0700

    create main program and use function hello

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
 
 public class Main {
+	import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		
+		Hello hello = new Hello();
+		hello.setName(str);
+		System.out.println(hello.getName);
+	}
+
+}
 
 }
